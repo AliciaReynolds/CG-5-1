c9c4f2821b888e8ea7613af62b94f6fbf2de1d4a
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-09 21:32:05 -0500

    Inital code

--- CG 5-1/Program.cs
+++ CG 5-1/Program.cs
@@ -6,7 +6,26 @@ namespace CG_5_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            //Have the user input their first name
+
+            Console.WriteLine("Hello.  Please enter your first name.");
+            string First = Console.ReadLine();
+
+            //Have the user input their last name
+
+            Console.WriteLine("Thank you.  Now please enter your last name.");
+            string Last = Console.ReadLine();
+
+
+            //Create method that combines the names and outputs them
+            Combo(First);
+            Combo(Last);
+
+
+         }
+        private static string Combo(string First, string Last)
+    { Console.WriteLine($"Nice to meet you {First} {Last}");
     }
+
+
 }
